32bc3c14308282752ab7515fe776ea05b66323a1
Влад Язиков <vladzaraza@gmail.com>
2023-11-21 12:40:49 +0200

    starter

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  server: {
+    open: true, // Автоматически открывать браузер при запуске сервера
+  },
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+    },
+  },
+  base: "/wow/",
+});
